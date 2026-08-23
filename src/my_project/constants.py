c1327e01d17675d5df39e2521f0505328b17361f
AukeB <afbruinsma@gmail.com>
2026-08-23 16:44:32 +0200

    Updated

--- src/my_project/constants.py
+++ src/my_project/constants.py
@@ -3,7 +3,3 @@
 from pathlib import Path
 
 CONFIG_PATH = Path("src/my_project/configs/config.yaml")
-
-# If PyGame is used:
-# Determines the ratio between the PyGame window resolution and the screen resolution
-WINDOW_SIZE_SCREEN_FRACTION = 0.9
